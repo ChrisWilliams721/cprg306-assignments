c8e45d978af20a647d31145bbfcb5d66fcf2e002
Chris Williams <cchriswilliams721@gmail.com>
2024-09-10 14:21:24 -0600

    Assignment Week 2 completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Student() {
+    return(
+        <main>
+            <p>Christopher Williams</p>
+            <Link href="https://github.com/ChrisWilliams721/cprg306-assignments">http://github.com</Link>
+        </main>
+    )
+};
