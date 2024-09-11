bc84eaf0ac50a7b3b4d57d8abed22b77e5743b6b
Chris Williams <cchriswilliams721@gmail.com>
2024-09-11 09:50:27 -0600

    Fixed errors

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link"
 
-export default function root() {
+export default function Root() {
   return(
     <main>
       <h1 className="text-5xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link className="text-2xl  text-cyan-500 hover:text-cyan-300 nderline"  href="./week-2" >Week 2 Assignment</Link>
+      <Link className="text-2xl  text-cyan-500 hover:text-cyan-300 "  href="./week-2" >Week 2 Assignment</Link>
     </main>
   )
 };
