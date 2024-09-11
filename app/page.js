b84ddcd8510aab936af9610ff20a37e46cbbc83e
Chris Williams <cchriswilliams721@gmail.com>
2024-09-11 09:22:47 -0600

    added link styling

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function root() {
   return(
     <main>
       <h1 className="text-5xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link className="text-2xl" href="./week-2">Week 2 Assignment</Link>
+      <Link className="text-2xl  text-cyan-500 hover:text-cyan-300 nderline"  href="./week-2" >Week 2 Assignment</Link>
     </main>
   )
 };
