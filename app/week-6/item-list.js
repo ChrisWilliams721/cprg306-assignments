5523bfb2b4f4449bb0f046e33a3fc0cccbf3ee84
Chris Williams <cchriswilliams721@gmail.com>
2024-10-22 11:58:00 -0600

    updated week-6

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,46 +1,54 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Item from './item';
-import itemsData from './items.json';
+import itemsData from './item.json';
 
 export default function ItemList() {
-  const [sortBy, setSortBy] = useState('name'); // Set default sort by 'name'
-  const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    let sortedItems = [...itemsData];
+  const [sortBy, setSortBy] = useState('name');
+  const [filterCategory, setFilterCategory] = useState('all');
 
-    // Sort items based on sortBy state
-    if (sortBy === 'name') {
-      sortedItems.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (sortBy === 'category') {
-      sortedItems.sort((a, b) => a.category.localeCompare(b.category));
+  const [itemlist, setItemlist] = useState(
+    itemsData.map((item) => ({...item}))
+  );
+
+  const sortedItems = [...itemlist].sort((a,b) => {
+    if(sortBy === 'name') {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === 'quantity'){
+      return a.quantity - b.quantity;
     }
-
-    setItems(sortedItems);
-  }, [sortBy]);
-
-  return (
+    return 0;
+  });
+  const filteredItems = sortedItems.filter(item => {
+    if (filterCategory === 'all') {
+      return true; 
+    }
+    return item.category === filterCategory;
+  });
+  return(
     <div>
-      <div>
-        <label>Sort By: </label>
-        <select onChange={(e) => setSortBy(e.target.value)} value={sortBy}>
-          <option value="name">Name</option>
-          <option value="category">Category</option>
-        </select>
-      </div>
+      <h1>Shopping List</h1>
+
+      <label>Filter by category: </label>
+      <select value={filterCategory} onChange={(event) => setFilterCategory(event.target.value)}>
+        <option value={"all"}>All category</option>
+        <option value={"dairy"}>dairy</option>
+        <option value={"bakery"}>bakery</option>
+        <option value={"produce"}>produce</option>
+        <option value={"meat"}>meat</option>
+        <option value={"canned goods"}>canned foods</option>
+        <option value={"dry goods"}>dry goods</option>
+        <option value={"household"}>household</option>
+      </select>
 
       <ul>
-        {items.map((item) => (
-          <Item
-            key={item.id}
-            name={item.name}
-            quantity={item.quantity}
-            category={item.category}
-          />
+        {filteredItems.map((item) => (
+           <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} />
         ))}
       </ul>
     </div>
-  );
+  )
+  
 }
