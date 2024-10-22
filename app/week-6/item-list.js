b2c5adc6134f335676379b0812ad13f40a9edc7f
Chris Williams <cchriswilliams721@gmail.com>
2024-10-22 12:40:41 -0600

    styling updated
    Removed unwanted code and updated the styling

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -7,7 +7,6 @@ import itemsData from './item.json';
 export default function ItemList() {
 
   const [sortBy, setSortBy] = useState('name');
-  const [filterCategory, setFilterCategory] = useState('all');
 
   const [itemlist, setItemlist] = useState(
     itemsData.map((item) => ({...item}))
@@ -16,36 +15,30 @@ export default function ItemList() {
   const sortedItems = [...itemlist].sort((a,b) => {
     if(sortBy === 'name') {
       return a.name.localeCompare(b.name);
-    } else if (sortBy === 'quantity'){
-      return a.quantity - b.quantity;
+    } else if (sortBy === 'category'){
+      return a.category.localeCompare(b.category);
     }
     return 0;
   });
-  const filteredItems = sortedItems.filter(item => {
-    if (filterCategory === 'all') {
-      return true; 
-    }
-    return item.category === filterCategory;
-  });
   return(
-    <div>
-      <label>Filter by category: </label>
-      <select
-      className="text-sm text-black"
-      value={filterCategory} 
-      onChange={(event) => setFilterCategory(event.target.value)}>
-        <option value={"all"}>All category</option>
-        <option value={"dairy"}>dairy</option>
-        <option value={"bakery"}>bakery</option>
-        <option value={"produce"}>produce</option>
-        <option value={"meat"}>meat</option>
-        <option value={"canned goods"}>canned foods</option>
-        <option value={"dry goods"}>dry goods</option>
-        <option value={"household"}>household</option>
-      </select>
+    <div >
+      <label>Sort by: </label>
+
+      <button
+        className="text-sm text-white bg-lime-500 hover:bg-lime-700 px-2 mx-2"
+        onClick={() => setSortBy('name')}
+      >
+        Name
+      </button>
+      <button
+        className="text-sm text-white bg-lime-500 hover:bg-lime-700 px-2 mx-2"
+        onClick={() => setSortBy('category')}
+      >
+        category
+      </button>
 
       <ul>
-        {filteredItems.map((item) => (
+        {sortedItems.map((item) => (
            <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} />
         ))}
       </ul>
--- app/week-6/item.js
+++ app/week-6/item.js
@@ -1,11 +1,12 @@
 export default function Item({name, quantity,category}){
     return (
-        <li className="flex justify-between items-center p-4 border-b border-gray-300">
+        <li className="flex justify-between items-center p-4 border-b border-gray-300 bg-stone-700 m-8 rounded">
           <div>
-            <h3 className="text-lg font-semibold text-blue-600 hover:text-blue-900">{name}</h3>
-            <p className="text-sm text-black">Category: {category}</p>
+            <h3 className="text-lg font-semibold text-white-600 hover:text-gray-600">{name}</h3>
+            <p className="text-sm text-white-600">Category: {category}</p>
+            <p className="text-lg   text-white-600">Buy {quantity} in {category}</p>
           </div>
-          <p className="text-lg font-bold  text-blue-600">x{quantity}</p>
+          
         </li>
       );
 };
