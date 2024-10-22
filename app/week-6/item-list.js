091e436df734c35a4cd0df9664a5265d80e072fb
Chris Williams <cchriswilliams721@gmail.com>
2024-10-22 13:25:21 -0600

    added grouping reduce func to group by category

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,48 +1,83 @@
 "use client";
 
-import { useState } from 'react';
-import Item from './item';
-import itemsData from './item.json';
+import { useState } from "react";
+import Item from "./item";
+import itemsData from "./item.json";
 
 export default function ItemList() {
-
-  const [sortBy, setSortBy] = useState('name');
+  const [sortBy, setSortBy] = useState("name");
 
   const [itemlist, setItemlist] = useState(
-    itemsData.map((item) => ({...item}))
+    itemsData.map((item) => ({ ...item }))
   );
+  const grouped = itemlist.reduce((groupedItems, item) => {
+    const category = item.category;
+    if (groupedItems[category] == null) groupedItems[category] = [];
+    groupedItems[category].push(item);
+    return groupedItems;
+  }, {});
 
-  const sortedItems = [...itemlist].sort((a,b) => {
-    if(sortBy === 'name') {
-      return a.name.localeCompare(b.name);
-    } else if (sortBy === 'category'){
-      return a.category.localeCompare(b.category);
-    }
-    return 0;
-  });
-  return(
-    <div >
+  const SortedItems = () => {
+    return [...itemlist]
+      .sort((a, b) => {
+        if (sortBy === "name") {
+          return a.name.localeCompare(b.name);
+        } else if (sortBy === "category") {
+          return a.category.localeCompare(b.category);
+        }
+        return 0;
+      })
+      .map((item) => (
+        <Item
+          key={item.id}
+          name={item.name}
+          quantity={item.quantity}
+          category={item.category}
+        />
+      ));
+  };
+  const GroupedItems = () => {
+    const sortedCategories = Object.keys(grouped).sort();
+    return sortedCategories.map((category) => (
+      <li key={category}>
+        <h2 className="font-bold">{category}</h2>
+        <ul>
+          {grouped[category].map((item) => (
+            <Item
+              key={item.id}
+              name={item.name}
+              quantity={item.quantity}
+              category={item.category}
+            />
+          ))}
+        </ul>
+      </li>
+    ));
+  };
+  return (
+    <div>
       <label>Sort by: </label>
 
       <button
         className="text-sm text-white bg-lime-500 hover:bg-lime-700 px-2 mx-2"
-        onClick={() => setSortBy('name')}
+        onClick={() => setSortBy("name")}
       >
         Name
       </button>
       <button
         className="text-sm text-white bg-lime-500 hover:bg-lime-700 px-2 mx-2"
-        onClick={() => setSortBy('category')}
+        onClick={() => setSortBy("category")}
       >
         category
       </button>
+      <button
+        className="text-sm text-white bg-lime-500 hover:bg-lime-700 px-2 mx-2"
+        onClick={() => setSortBy("grouped")}
+      >
+        Grouped
+      </button>
 
-      <ul>
-        {sortedItems.map((item) => (
-           <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} />
-        ))}
-      </ul>
+      <ul>{sortBy === "grouped" ? GroupedItems() : SortedItems()}</ul>
     </div>
-  )
-  
+  );
 }
